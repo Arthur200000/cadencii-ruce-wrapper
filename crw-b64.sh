ddbe2dedf078519b7f3f681768e8a7c7c26f2df4
Arthur Wang <arthur200126@gmail.com>
2014-08-28 09:32:35 +0800

    aaa

--- crw-b64.sh
+++ crw-b64.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # convert.c
 
+crw64='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+# convert
+crw-b64index(){ printf ${crw64:$1:1}; }
+
+# main, Base64ForUtau
 crw-b64(){
   read f
   # Rounding, now it's the int i.
@@ -9,11 +15,7 @@ crw-b64(){
   [[ "$f" -lt "0" ]] && ((f += 4096))
   f_low=$(( $f & 63 ))
   f_high=$(( ($f & 0xFC0) >> 6 ))
-  # Time to encode. Now it is f*cking-bc-dependent. Will consider using cut+hexdump.
-  # We need to add backslashes back to the oct string. Going to bed.
-  printf "%b" "$(_crw-intoct $f_low)$(_crw-intoct $f_high)" | base64
-}
-
-_crw-intoct(){
-  printf '%o' $(echo "obase=8;ibase=10;$*" | bc)
+  # Time to encode. Can be slow.
+  crw-b64index $f_low
+  crw-b64index $f_high
 }
